0b86201a600308992fabeb956af4585e505efed1
Louise Lista <louise@roraimadevs.com>
2023-05-30 09:46:44 -0400

    fix: create an account button is now  grey

--- src/keycloak-theme/login/pages/Login.tsx
+++ src/keycloak-theme/login/pages/Login.tsx
@@ -63,9 +63,13 @@ export default function Login(props: PageProps<Extract<KcContext, { pageId: "log
                 realm.registrationAllowed &&
                 !registrationDisabled && (
                     <div id="kc-registration" className="instruction">
-                        <div className={clsx(getClassName("kcFormButtonsClass"), "!p-0")}>
-                            <button onClick={() => window.location.href = url.registrationUrl} className={"btn-primary !h-11 w-full"}>
-                                {msg("doRegister")}
+                        <div className={clsx(getClassName("kcFormButtonsClass"), "!p-0 ")}>
+                            <button onClick={() => window.location.href = url.registrationUrl} className={"btn !h-11 w-full bg-dark-400 hover:bg-dark-300 !text-white"}>
+                                <svg className={"h-5 mb-0.5 mr-2 "} viewBox="0 0 24 24" focusable="false" aria-hidden="true">
+                                    <path d="M12 3C9.61303 3 7.32394 3.94821 5.63606 5.63606C3.94817 7.32391 3 9.61303 3 12C3 14.387 3.94821 16.6761 5.63606 18.3639C7.32391 20.0518 9.61303 21 12 21C14.387 21 16.6761 20.0518 18.3639 18.3639C20.0518 16.6761 21 14.387 21 12C21 10.4202 20.5841 8.86817 19.7942 7.5C19.0043 6.13185 17.8681 4.9956 16.5001 4.20583C15.1319 3.41592 13.58 3.00007 12.0001 3.00007L12 3ZM7.1208 17.969C7.88225 16.9574 8.93931 16.2075 10.1457 15.823C11.352 15.4385 12.648 15.4385 13.8543 15.823C15.0607 16.2075 16.1179 16.9574 16.8792 17.969C15.5031 19.0966 13.7789 19.7127 12 19.7127C10.2211 19.7127 8.49669 19.0965 7.1208 17.969ZM17.8083 17.069C16.8827 15.8943 15.6226 15.0281 14.1945 14.5845C12.7663 14.141 11.2371 14.141 9.80897 14.5845C8.38082 15.028 7.1208 15.8943 6.19509 17.069C4.88727 15.5737 4.20669 13.6315 4.29497 11.647C4.38336 9.6624 5.23394 7.78851 6.6696 6.41537C8.10525 5.04233 10.0152 4.27594 12.0017 4.27594C13.9882 4.27594 15.8981 5.04228 17.3338 6.41537C18.7694 7.78848 19.62 9.6624 19.7085 11.647C19.7968 13.6317 19.1162 15.5738 17.8083 17.069Z" fill="currentColor"></path>
+                                    <path d="M12 5.89282C10.9771 5.89282 9.996 6.29923 9.27257 7.02257C8.54923 7.74591 8.14282 8.72702 8.14282 9.75C8.14282 10.773 8.54923 11.754 9.27257 12.4774C9.99591 13.2008 10.977 13.6072 12 13.6072C13.023 13.6072 14.004 13.2008 14.7274 12.4774C15.4508 11.7541 15.8572 10.773 15.8572 9.75C15.8572 8.72702 15.4508 7.746 14.7274 7.02257C14.0041 6.29923 13.023 5.89282 12 5.89282ZM12 12.3214C11.318 12.3214 10.6639 12.0505 10.1817 11.5682C9.69939 11.086 9.4285 10.4319 9.4285 9.7499C9.4285 9.06787 9.69945 8.41382 10.1817 7.93155C10.6639 7.44929 11.318 7.1784 12 7.1784C12.682 7.1784 13.3361 7.44934 13.8183 7.93155C14.3006 8.41376 14.5715 9.06787 14.5715 9.7499C14.5715 10.4319 14.3006 11.086 13.8183 11.5682C13.3361 12.0505 12.682 12.3214 12 12.3214Z" fill="currentColor"></path>
+                                </svg>
+                                Create an Account
                             </button>
                         </div>
                     </div>
@@ -184,12 +188,12 @@ export default function Login(props: PageProps<Extract<KcContext, { pageId: "log
                                             getClassName("kcButtonPrimaryClass"),
                                             getClassName("kcButtonBlockClass"),
                                             getClassName("kcButtonLargeClass"),
-                                            '!mt-0 !h-11'
+                                            '!mt-0 !h-11 px-6'
                                         )}
                                         name="login"
                                         id="kc-login"
                                         type="submit"
-                                        value={msgStr("doLogIn")}
+                                        value="Log In"
                                         disabled={isLoginButtonDisabled}
                                     />
                                 </div>
