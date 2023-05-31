c48a67827ab21778b18fd9f199e2e4c46defd0fd
Louise Lista <louise@roraimadevs.com>
2023-05-31 09:54:00 -0400

    feat: set min char validation to the password confirm form

--- src/keycloak-theme/login/pages/LoginUpdatePassword.tsx
+++ src/keycloak-theme/login/pages/LoginUpdatePassword.tsx
@@ -3,6 +3,9 @@ import type { PageProps } from "keycloakify/login/pages/PageProps";
 import { useGetClassName } from "keycloakify/login/lib/useGetClassName";
 import type { KcContext } from "../kcContext";
 import type { I18n } from "../i18n";
+import { FormInputError } from "./shared/FormInputError";
+import { useState, useEffect, FormEventHandler } from "react";
+import { useConstCallback } from "keycloakify/tools/useConstCallback";
 
 export default function LoginUpdatePassword(props: PageProps<Extract<KcContext, { pageId: "login-update-password.ftl" }>, I18n>) {
     const { kcContext, i18n, doUseDefaultCss, Template, classes } = props;
@@ -15,6 +18,47 @@ export default function LoginUpdatePassword(props: PageProps<Extract<KcContext, 
     const { msg, msgStr } = i18n;
 
     const { url, messagesPerField, isAppInitiatedAction, username } = kcContext;
+    const [password, setPassword] = useState("");
+    const [passwordConfirm, setPasswordConfirm] = useState("");
+    const [errors, setErrors] = useState({} as {password: string, passwordConfirm: string});
+    const [wasSubmitted, setWasSubmitted] = useState(false);
+
+    const validatePassword = () => {
+        if (!password) {
+            return "Required";
+        } else if (password.length < 8) {
+            return "Password must be at least 8 characters";
+        }
+        return "";
+    };
+    const validatePasswordConfirm = () => {
+        if (!passwordConfirm) {
+            return "Required";
+        } else if (password!== passwordConfirm) {
+            return "Passwords do not match";
+        }
+        return "";      
+    };
+    const validateForm = () => {
+        setErrors({password: validatePassword(), passwordConfirm: validatePasswordConfirm()});
+    }
+    const onSubmit = useConstCallback<FormEventHandler<HTMLFormElement>>(e => {
+        e.preventDefault();
+        setWasSubmitted(true);
+        const formElement = e.target as HTMLFormElement;
+        if (formElement.checkValidity() === true) {
+            formElement.submit();
+        }
+        else {
+            validateForm();
+        }
+
+    });
+    useEffect(() => {
+        if (wasSubmitted) {
+            validateForm();
+        }
+    }, [password, passwordConfirm]);
 
     return (
         <Template {...{ kcContext, i18n, doUseDefaultCss, classes }} 
@@ -27,7 +71,7 @@ export default function LoginUpdatePassword(props: PageProps<Extract<KcContext, 
                     <p className="text-center">Create your new strong password</p>
                 </div>
             }>
-            <form id="kc-passwd-update-form" className={getClassName("kcFormClass")} action={url.loginAction} method="post">
+            <form id="kc-passwd-update-form" className={getClassName("kcFormClass")} action={url.loginAction} method="post"  onSubmit={(e) => { e.preventDefault(); e.stopPropagation(); onSubmit(e) }} noValidate>
                 <input
                     type="text"
                     id="username"
@@ -54,7 +98,13 @@ export default function LoginUpdatePassword(props: PageProps<Extract<KcContext, 
                             autoFocus
                             autoComplete="new-password"
                             className={getClassName("kcInputClass")}
+                            onChange={ (e) => setPassword((e.target as HTMLInputElement).value) }
+                            required
+                            minLength={8}
                         />
+                        {!!errors.password ? (                              
+                            <FormInputError message={errors.password} />   
+                        ) : null}
                     </div>
                 </div>
 
@@ -72,7 +122,13 @@ export default function LoginUpdatePassword(props: PageProps<Extract<KcContext, 
                             autoComplete="new-password"
                             placeholder="Confirm Password"
                             className={getClassName("kcInputClass")}
+                            onChange={ (e) => setPasswordConfirm((e.target as HTMLInputElement).value) }
+                            required
+                            minLength={8}
                         />
+                        {!!errors.passwordConfirm ? (                               
+                            <FormInputError message={errors.passwordConfirm} /> 
+                        ) : null}
                     </div>
                 </div>
 
