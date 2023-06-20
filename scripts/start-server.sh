4f63a11f48770047603b0cfddbbf49061852a5b6
Stéphane GUILLY <stephane.guilly@lvr-fleet.fr>
2023-06-20 14:58:31 +0200

    feat(scripts): use variable for admin user

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -2,6 +2,7 @@
 
 #VERSION="20.0.1"
 VERSION="21.1.1"
+ADMIN_USER="admin"
 echo $PWD
 
 docker rm keycloak-testing-container || true
@@ -9,10 +10,13 @@ docker rm keycloak-testing-container || true
 docker run \
    -p 8080:8080 \
    --name keycloak-testing-container \
-   -e KEYCLOAK_ADMIN=admin \
-   -e KEYCLOAK_ADMIN_PASSWORD=admin \
+   -e KEYCLOAK_ADMIN=$ADMIN_USER \
+   -e KEYCLOAK_ADMIN_PASSWORD=$ADMIN_USER \
    -e JAVA_OPTS=-Dkeycloak.profile=preview \
    -v $PWD/build_keycloak/src/main/resources/theme/b2pconnect:/opt/keycloak/themes/b2pconnect:rw \
    -v $PWD/realm:/opt/keycloak/data/import:rw \
    -it quay.io/keycloak/keycloak:$VERSION \
    start-dev --import-realm
+
+
+
