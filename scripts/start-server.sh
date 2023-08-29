34298eaca813e662bff00c8c22d7c2deb1b98d43
Stéphane GUILLY <stephane.guilly@lvr-fleet.fr>
2023-08-29 11:10:36 +0200

    feat(makefile): use theme jar

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -13,8 +13,8 @@ docker run \
    -e KEYCLOAK_ADMIN=$ADMIN_USER \
    -e KEYCLOAK_ADMIN_PASSWORD=$ADMIN_USER \
    -e JAVA_OPTS=-Dkeycloak.profile=preview \
-   -v $PWD/build_keycloak/src/main/resources/theme/b2pconnect:/opt/keycloak/themes/b2pconnect:rw \
-   -v $PWD/realm:/opt/keycloak/data/import:rw \
+   -v "$PWD"/build_keycloak/target:/opt/keycloak/providers:rw \
+   -v "$PWD"/realm:/opt/keycloak/data/import:rw \
    -it quay.io/keycloak/keycloak:$VERSION \
    start-dev --import-realm
 
