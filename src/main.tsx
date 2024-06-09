afb077b30a82454a772de7c9fba954411cae0211
Joseph Garrone <joseph.garrone.gj@gmail.com>
2024-06-09 12:33:12 +0200

    Update

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { StrictMode, lazy, Suspense } from "react";
 // The following block can be uncommented to test a specific page with `yarn dev`
 // Don't forget to comment back or your bundle size will increase
 /*
-import { getKcContextMock } from "./login/PageStory";
+import { getKcContextMock } from "./login/KcPageStory";
 
 if (import.meta.env.DEV) {
     window.kcContext = getKcContextMock({
