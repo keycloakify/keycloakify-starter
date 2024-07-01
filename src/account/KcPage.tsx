e4d4562b0e78b9b27afdb7b756229af21d7543c7
Joseph Garrone <joseph.garrone.gj@gmail.com>
2024-07-01 21:50:35 +0200

    Demonstrate how to call the account API

--- src/account/KcPage.tsx
+++ src/account/KcPage.tsx
@@ -1,15 +1,45 @@
-import { Suspense, lazy } from "react";
+import { useEffect, Suspense, lazy } from "react";
 import type { ClassKey } from "keycloakify/account";
 import type { KcContext } from "./KcContext";
 import { useI18n } from "./i18n";
 import DefaultPage from "keycloakify/account/DefaultPage";
 import Template from "keycloakify/account/Template";
+import { OidcProvider, useOidc } from "./oidc";
 const Password = lazy(() => import("./pages/Password"));
 
 export default function KcPage(props: { kcContext: KcContext }) {
+    return (
+        <OidcProvider>
+            <ContextualizedKcPage {...props} />
+        </OidcProvider>
+    );
+}
+
+function ContextualizedKcPage(props: { kcContext: KcContext }) {
     const { kcContext } = props;
 
     const { i18n } = useI18n({ kcContext });
+
+    const { oidcTokens, params } = useOidc();
+
+    useEffect(() => {
+
+        if( import.meta.env.DEV ){
+            return;
+        }
+
+        fetch(`${params.issuerUri}/account/?userProfileMetadata=true`, {
+            headers: {
+                Authorization: `Bearer ${oidcTokens.accessToken}`,
+                "Content-Type": "application/json"
+            },
+
+        })
+            .then(response => response.json())
+            .then(console.log);
+
+    }, []);
+
 
     return (
         <Suspense>
--- src/account/pages/Password.tsx
+++ src/account/pages/Password.tsx
@@ -1,9 +1,9 @@
-import { useState } from "react";
 import { clsx } from "keycloakify/tools/clsx";
 import { getKcClsx } from "keycloakify/account/lib/kcClsx";
 import type { PageProps } from "keycloakify/account/pages/PageProps";
 import type { KcContext } from "../KcContext";
 import type { I18n } from "../i18n";
+import { useOidc } from "../oidc";
 
 export default function Password(props: PageProps<Extract<KcContext, { pageId: "password.ftl" }>, I18n>) {
     const { kcContext, i18n, doUseDefaultCss, Template } = props;
@@ -18,192 +18,52 @@ export default function Password(props: PageProps<Extract<KcContext, { pageId: "
         classes
     });
 
-    const { url, password, account, stateChecker } = kcContext;
+    const { msg } = i18n;
 
-    const { msgStr, msg } = i18n;
-
-    const [currentPassword, setCurrentPassword] = useState("");
-    const [newPassword, setNewPassword] = useState("");
-    const [newPasswordConfirm, setNewPasswordConfirm] = useState("");
-    const [newPasswordError, setNewPasswordError] = useState("");
-    const [newPasswordConfirmError, setNewPasswordConfirmError] = useState("");
-    const [hasNewPasswordBlurred, setHasNewPasswordBlurred] = useState(false);
-    const [hasNewPasswordConfirmBlurred, setHasNewPasswordConfirmBlurred] = useState(false);
-
-    const checkNewPassword = (newPassword: string) => {
-        if (!password.passwordSet) {
-            return;
-        }
-
-        if (newPassword === currentPassword) {
-            setNewPasswordError(msgStr("newPasswordSameAsOld"));
-        } else {
-            setNewPasswordError("");
-        }
-    };
-
-    const checkNewPasswordConfirm = (newPasswordConfirm: string) => {
-        if (newPasswordConfirm === "") {
-            return;
-        }
-
-        if (newPassword !== newPasswordConfirm) {
-            setNewPasswordConfirmError(msgStr("passwordConfirmNotMatch"));
-        } else {
-            setNewPasswordConfirmError("");
-        }
-    };
+    const { goToAuthServer } = useOidc();
 
     return (
         <Template
             {...{
-                kcContext: {
-                    ...kcContext,
-                    message: (() => {
-                        if (newPasswordError !== "") {
-                            return {
-                                type: "error",
-                                summary: newPasswordError
-                            };
-                        }
-
-                        if (newPasswordConfirmError !== "") {
-                            return {
-                                type: "error",
-                                summary: newPasswordConfirmError
-                            };
-                        }
-
-                        return kcContext.message;
-                    })()
-                },
+                kcContext,
                 i18n,
                 doUseDefaultCss,
                 classes
             }}
             active="password"
         >
+            {
+                /* NOTE: Since we can't have the password policies we simply create a button that
+                 * redirects to the login-update-password.ftl page of the login theme.
+                 * See: https://github.com/user-attachments/assets/6de0f904-afa9-4ccc-b0d3-88c07f1e80d0
+                 */
+            }
+
             <div className="row">
                 <div className="col-md-10">
                     <h2>{msg("changePasswordHtmlTitle")}</h2>
                 </div>
-                <div className="col-md-2 subtitle">
-                    <span className="subtitle">{msg("allFieldsRequired")}</span>
-                </div>
             </div>
-
-            <form action={url.passwordUrl} className="form-horizontal" method="post">
-                <input
-                    type="text"
-                    id="username"
-                    name="username"
-                    value={account.username ?? ""}
-                    autoComplete="username"
-                    readOnly
-                    style={{ display: "none" }}
-                />
-
-                {password.passwordSet && (
-                    <div className="form-group">
-                        <div className="col-sm-2 col-md-2">
-                            <label htmlFor="password" className="control-label">
-                                {msg("password")}
-                            </label>
-                        </div>
-                        <div className="col-sm-10 col-md-10">
-                            <input
-                                type="password"
-                                className="form-control"
-                                id="password"
-                                name="password"
-                                autoFocus
-                                autoComplete="current-password"
-                                value={currentPassword}
-                                onChange={event => setCurrentPassword(event.target.value)}
-                            />
-                        </div>
-                    </div>
-                )}
-
-                <input type="hidden" id="stateChecker" name="stateChecker" value={stateChecker} />
-
-                <div className="form-group">
-                    <div className="col-sm-2 col-md-2">
-                        <label htmlFor="password-new" className="control-label">
-                            {msg("passwordNew")}
-                        </label>
-                    </div>
-                    <div className="col-sm-10 col-md-10">
-                        <input
-                            type="password"
-                            className="form-control"
-                            id="password-new"
-                            name="password-new"
-                            autoComplete="new-password"
-                            value={newPassword}
-                            onChange={event => {
-                                const newPassword = event.target.value;
-
-                                setNewPassword(newPassword);
-                                if (hasNewPasswordBlurred) {
-                                    checkNewPassword(newPassword);
-                                }
-                            }}
-                            onBlur={() => {
-                                setHasNewPasswordBlurred(true);
-                                checkNewPassword(newPassword);
-                            }}
-                        />
-                    </div>
-                </div>
-
-                <div className="form-group">
-                    <div className="col-sm-2 col-md-2">
-                        <label htmlFor="password-confirm" className="control-label two-lines">
-                            {msg("passwordConfirm")}
-                        </label>
-                    </div>
-
-                    <div className="col-sm-10 col-md-10">
-                        <input
-                            type="password"
-                            className="form-control"
-                            id="password-confirm"
-                            name="password-confirm"
-                            autoComplete="new-password"
-                            value={newPasswordConfirm}
-                            onChange={event => {
-                                const newPasswordConfirm = event.target.value;
-
-                                setNewPasswordConfirm(newPasswordConfirm);
-                                if (hasNewPasswordConfirmBlurred) {
-                                    checkNewPasswordConfirm(newPasswordConfirm);
-                                }
-                            }}
-                            onBlur={() => {
-                                setHasNewPasswordConfirmBlurred(true);
-                                checkNewPasswordConfirm(newPasswordConfirm);
-                            }}
-                        />
-                    </div>
-                </div>
+            <form className="form-horizontal">
 
                 <div className="form-group">
                     <div id="kc-form-buttons" className="col-md-offset-2 col-md-10 submit">
                         <div>
                             <button
-                                disabled={newPasswordError !== "" || newPasswordConfirmError !== ""}
-                                type="submit"
                                 className={kcClsx("kcButtonClass", "kcButtonPrimaryClass", "kcButtonLargeClass")}
-                                name="submitAction"
-                                value="Save"
+                                onClick={() => goToAuthServer({
+                                    extraQueryParams: { kc_action: "UPDATE_PASSWORD" }
+                                })}
                             >
-                                {msg("doSave")}
+                                {msg("changePasswordHtmlTitle")}
                             </button>
                         </div>
                     </div>
                 </div>
+
             </form>
+
+
         </Template>
     );
 }
