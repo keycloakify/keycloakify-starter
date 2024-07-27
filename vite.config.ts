a2d7634fe5b66885914879915dab513cf6198a4c
Joseph Garrone <joseph.garrone.gj@gmail.com>
2024-07-27 17:11:30 +0200

    Move the entrypoint into kc.gen.tsx

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,10 @@ import { keycloakify } from "keycloakify/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), keycloakify({
-        accountThemeImplementation: "none"
-    })]
+    plugins: [
+        react(),
+        keycloakify({
+            accountThemeImplementation: "none"
+        })
+    ]
 });
