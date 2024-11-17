8078df3d5f37f71a2709e59c32f4e33970951d6a
Keloran <keloran@develbox.info>
2024-11-17 23:57:20 +0000

    hide the username and email

--- src/login/KcPageStory.tsx
+++ src/login/KcPageStory.tsx
@@ -6,37 +6,37 @@ import type { KcContextExtension, KcContextExtensionPerPage } from "./KcContext"
 import { themeNames, kcEnvDefaults } from "../kc.gen";
 
 const kcContextExtension: KcContextExtension = {
-    themeName: themeNames[0],
-    properties: {
-        ...kcEnvDefaults
-    }
+  themeName: themeNames[0],
+  properties: {
+    ...kcEnvDefaults
+  }
 };
 const kcContextExtensionPerPage: KcContextExtensionPerPage = {};
 
 export const { getKcContextMock } = createGetKcContextMock({
-    kcContextExtension,
-    kcContextExtensionPerPage,
-    overrides: {},
-    overridesPerPage: {}
+  kcContextExtension,
+  kcContextExtensionPerPage,
+  overrides: {},
+  overridesPerPage: {}
 });
 
 export function createKcPageStory<PageId extends KcContext["pageId"]>(params: {
-    pageId: PageId;
+  pageId: PageId;
 }) {
-    const { pageId } = params;
+  const { pageId } = params;
 
-    function KcPageStory(props: {
-        kcContext?: DeepPartial<Extract<KcContext, { pageId: PageId }>>;
-    }) {
-        const { kcContext: overrides } = props;
+  function KcPageStory(props: {
+    kcContext?: DeepPartial<Extract<KcContext, { pageId: PageId }>>;
+  }) {
+    const { kcContext: overrides } = props;
 
-        const kcContextMock = getKcContextMock({
-            pageId,
-            overrides
-        });
+    const kcContextMock = getKcContextMock({
+      pageId,
+      overrides
+    });
 
-        return <KcPage kcContext={kcContextMock} />;
-    }
+    return <KcPage kcContext={kcContextMock} />;
+  }
 
-    return { KcPageStory };
+  return { KcPageStory };
 }
