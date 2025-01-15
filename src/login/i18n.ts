55fc480763fdd9ef037c80bdc25bc116e22310de
Paul Werner <52678724+paulwer@users.noreply.github.com>
2025-01-15 20:05:47 +0100

    chore/i18n: additional languages for footer (#11)

--- src/login/i18n.ts
+++ src/login/i18n.ts
@@ -26,6 +26,36 @@ const { useI18n, ofTypeI18n } = i18nBuilder
             footerDataProtectionTitle: "Informativa sulla Privacy",
             footerCookiePreferencesTitle: "Impostazioni dei Cookie",
         },
+        es: {
+            footerImprintTitle: "Aviso Legal",
+            footerDataProtectionTitle: "Protección de Datos",
+            footerCookiePreferencesTitle: "Preferencias de Cookies"
+        },
+        cs: {
+            footerImprintTitle: "Tiráž",
+            footerDataProtectionTitle: "Ochrana Osobních Údajů",
+            footerCookiePreferencesTitle: "Nastavení Cookies"
+        },
+        nl: {
+            footerImprintTitle: "Colofon",
+            footerDataProtectionTitle: "Gegevensbescherming",
+            footerCookiePreferencesTitle: "Cookievoorkeuren"
+        },
+        pl: {
+            footerImprintTitle: "Nota Prawna",
+            footerDataProtectionTitle: "Ochrona Danych",
+            footerCookiePreferencesTitle: "Preferencje Plików Cookie"
+        },
+        ru: {
+            footerImprintTitle: "Выходные Данные",
+            footerDataProtectionTitle: "Защита Данных",
+            footerCookiePreferencesTitle: "Настройки Файлов Cookie"
+        },
+        sv: {
+            footerImprintTitle: "Impressum",
+            footerDataProtectionTitle: "Dataskydd",
+            footerCookiePreferencesTitle: "Cookie-inställningar"
+        }
     })
     .build();
 
