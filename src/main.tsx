a1ae40aed4c07ce6e8bac4721794fca4b0d1b948
waynemorphic <wayneklyde@gmail.com>
2025-02-07 13:36:16 +0300

    chore: Commented back dev mode code to reduce bundle size

--- src/main.tsx
+++ src/main.tsx
@@ -1,28 +1,25 @@
 import { createRoot } from "react-dom/client";
-import { lazy, StrictMode, Suspense } from "react";
+import { StrictMode } from "react";
+import { KcPage } from "./kc.gen";
 
 // The following block can be uncommented to test a specific page with `yarn dev`
 // Don't forget to comment back or your bundle size will increase
-import { getKcContextMock } from "./login/KcPageStory";
-
-if (import.meta.env.DEV) {
-    window.kcContext = getKcContextMock({
-        pageId: "login.ftl",
-        overrides: {}
-    });
-}
-
-const KcLoginThemePage = lazy(() => import("./login/KcPage"));
+// import { getKcContextMock } from "./login/KcPageStory";
+//
+// if (import.meta.env.DEV) {
+//     window.kcContext = getKcContextMock({
+//         pageId: "login.ftl",
+//         overrides: {}
+//     });
+// }
+//
+// const KcLoginThemePage = lazy(() => import("./login/KcPage"));
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <Suspense>
-            {(() => {
-                switch (window.kcContext?.themeType) {
-                    case "login":
-                        return <KcLoginThemePage kcContext={window.kcContext} />;
-                }
-            })()}
-        </Suspense>
+            {!window.kcContext ? (
+                <h1>No Keycloak Context</h1>
+            ) : (
+                <KcPage kcContext={window.kcContext}/> )}
     </StrictMode>
 );
