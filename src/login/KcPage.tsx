70cc95df7a420269bef0621e7e3510bcef7005ae
garronej <joseph.garrone@protonmail.com>
2025-04-21 23:05:46 +0200

    Refactor

--- src/login/KcPage.tsx
+++ src/login/KcPage.tsx
@@ -5,6 +5,22 @@ import { I18nProvider } from "./i18n";
 import { KcClsxProvider } from "./_internals/useKcClsx";
 import { assert, type Equals } from "tsafe/assert";
 
+export default function KcPage(props: { kcContext: KcContext }) {
+    const { kcContext } = props;
+
+    return (
+        <KcClsxProvider doUseDefaultCss={true} classes={classes}>
+            <I18nProvider kcContext={kcContext}>
+                <Suspense>
+                    <Page kcContext={kcContext} />
+                </Suspense>
+            </I18nProvider>
+        </KcClsxProvider>
+    );
+}
+
+const classes = {} satisfies { [key in ClassKey]?: string };
+
 const Login = lazy(() => import("./pages/Login"));
 const Register = lazy(() => import("./pages/Register"));
 const Info = lazy(() => import("./pages/Info"));
@@ -52,174 +68,84 @@ const LoginIdpLinkConfirmOverride = lazy(
     () => import("./pages/LoginIdpLinkConfirmOverride")
 );
 
-export default function KcPage(props: { kcContext: KcContext }) {
+function Page(props: { kcContext: KcContext }) {
     const { kcContext } = props;
 
-    return (
-        <KcClsxProvider doUseDefaultCss={true} classes={classes}>
-            <I18nProvider kcContext={kcContext}>
-                <Suspense>
-                    {(() => {
-                        switch (kcContext.pageId) {
-                            case "login.ftl":
-                                return <Login kcContext={kcContext} />;
-                            case "register.ftl":
-                                return (
-                                    <Register
-                                        kcContext={kcContext}
-                                        doMakeUserConfirmPassword={true}
-                                    />
-                                );
-                            case "info.ftl":
-                                return <Info kcContext={kcContext} {...rest} />;
-                            case "error.ftl":
-                                return <Error kcContext={kcContext} {...rest} />;
-                            case "login-reset-password.ftl":
-                                return (
-                                    <LoginResetPassword kcContext={kcContext} {...rest} />
-                                );
-                            case "login-verify-email.ftl":
-                                return (
-                                    <LoginVerifyEmail kcContext={kcContext} {...rest} />
-                                );
-                            case "terms.ftl":
-                                return <Terms kcContext={kcContext} {...rest} />;
-                            case "login-oauth2-device-verify-user-code.ftl":
-                                return (
-                                    <LoginOauth2DeviceVerifyUserCode
-                                        kcContext={kcContext}
-                                        {...rest}
-                                    />
-                                );
-                            case "login-oauth-grant.ftl":
-                                return (
-                                    <LoginOauthGrant kcContext={kcContext} {...rest} />
-                                );
-                            case "login-otp.ftl":
-                                return <LoginOtp kcContext={kcContext} {...rest} />;
-                            case "login-username.ftl":
-                                return <LoginUsername kcContext={kcContext} {...rest} />;
-                            case "login-password.ftl":
-                                return <LoginPassword kcContext={kcContext} {...rest} />;
-                            case "webauthn-authenticate.ftl":
-                                return (
-                                    <WebauthnAuthenticate
-                                        kcContext={kcContext}
-                                        {...rest}
-                                    />
-                                );
-                            case "webauthn-register.ftl":
-                                return (
-                                    <WebauthnRegister kcContext={kcContext} {...rest} />
-                                );
-                            case "login-update-password.ftl":
-                                return (
-                                    <LoginUpdatePassword
-                                        kcContext={kcContext}
-                                        {...rest}
-                                    />
-                                );
-                            case "login-update-profile.ftl":
-                                return (
-                                    <LoginUpdateProfile kcContext={kcContext} {...rest} />
-                                );
-                            case "login-idp-link-confirm.ftl":
-                                return (
-                                    <LoginIdpLinkConfirm
-                                        kcContext={kcContext}
-                                        {...rest}
-                                    />
-                                );
-                            case "login-idp-link-email.ftl":
-                                return (
-                                    <LoginIdpLinkEmail kcContext={kcContext} {...rest} />
-                                );
-                            case "login-page-expired.ftl":
-                                return (
-                                    <LoginPageExpired kcContext={kcContext} {...rest} />
-                                );
-                            case "login-config-totp.ftl":
-                                return (
-                                    <LoginConfigTotp kcContext={kcContext} {...rest} />
-                                );
-                            case "logout-confirm.ftl":
-                                return <LogoutConfirm kcContext={kcContext} {...rest} />;
-                            case "idp-review-user-profile.ftl":
-                                return (
-                                    <IdpReviewUserProfile
-                                        kcContext={kcContext}
-                                        {...rest}
-                                    />
-                                );
-                            case "update-email.ftl":
-                                return <UpdateEmail kcContext={kcContext} {...rest} />;
-                            case "select-authenticator.ftl":
-                                return (
-                                    <SelectAuthenticator
-                                        kcContext={kcContext}
-                                        {...rest}
-                                    />
-                                );
-                            case "saml-post-form.ftl":
-                                return <SamlPostForm kcContext={kcContext} {...rest} />;
-                            case "delete-credential.ftl":
-                                return (
-                                    <DeleteCredential kcContext={kcContext} {...rest} />
-                                );
-                            case "code.ftl":
-                                return <Code kcContext={kcContext} {...rest} />;
-                            case "delete-account-confirm.ftl":
-                                return (
-                                    <DeleteAccountConfirm
-                                        kcContext={kcContext}
-                                        {...rest}
-                                    />
-                                );
-                            case "frontchannel-logout.ftl":
-                                return (
-                                    <FrontchannelLogout kcContext={kcContext} {...rest} />
-                                );
-                            case "login-recovery-authn-code-config.ftl":
-                                return (
-                                    <LoginRecoveryAuthnCodeConfig
-                                        kcContext={kcContext}
-                                        {...rest}
-                                    />
-                                );
-                            case "login-recovery-authn-code-input.ftl":
-                                return (
-                                    <LoginRecoveryAuthnCodeInput
-                                        kcContext={kcContext}
-                                        {...rest}
-                                    />
-                                );
-                            case "login-reset-otp.ftl":
-                                return <LoginResetOtp kcContext={kcContext} {...rest} />;
-                            case "login-x509-info.ftl":
-                                return <LoginX509Info kcContext={kcContext} {...rest} />;
-                            case "webauthn-error.ftl":
-                                return <WebauthnError kcContext={kcContext} {...rest} />;
-                            case "login-passkeys-conditional-authenticate.ftl":
-                                return (
-                                    <LoginPasskeysConditionalAuthenticate
-                                        kcContext={kcContext}
-                                        {...rest}
-                                    />
-                                );
-                            case "login-idp-link-confirm-override.ftl":
-                                return (
-                                    <LoginIdpLinkConfirmOverride
-                                        kcContext={kcContext}
-                                        {...rest}
-                                    />
-                                );
-                        }
-                        assert<Equals<typeof kcContext, never>>(false);
-                    })()}
-                </Suspense>
-            </I18nProvider>
-        </KcClsxProvider>
-    );
+    switch (kcContext.pageId) {
+        case "login.ftl":
+            return <Login kcContext={kcContext} />;
+        case "register.ftl":
+            return <Register kcContext={kcContext} doMakeUserConfirmPassword={true} />;
+        case "info.ftl":
+            return <Info kcContext={kcContext} {...rest} />;
+        case "error.ftl":
+            return <Error kcContext={kcContext} {...rest} />;
+        case "login-reset-password.ftl":
+            return <LoginResetPassword kcContext={kcContext} {...rest} />;
+        case "login-verify-email.ftl":
+            return <LoginVerifyEmail kcContext={kcContext} {...rest} />;
+        case "terms.ftl":
+            return <Terms kcContext={kcContext} {...rest} />;
+        case "login-oauth2-device-verify-user-code.ftl":
+            return <LoginOauth2DeviceVerifyUserCode kcContext={kcContext} {...rest} />;
+        case "login-oauth-grant.ftl":
+            return <LoginOauthGrant kcContext={kcContext} {...rest} />;
+        case "login-otp.ftl":
+            return <LoginOtp kcContext={kcContext} {...rest} />;
+        case "login-username.ftl":
+            return <LoginUsername kcContext={kcContext} {...rest} />;
+        case "login-password.ftl":
+            return <LoginPassword kcContext={kcContext} {...rest} />;
+        case "webauthn-authenticate.ftl":
+            return <WebauthnAuthenticate kcContext={kcContext} {...rest} />;
+        case "webauthn-register.ftl":
+            return <WebauthnRegister kcContext={kcContext} {...rest} />;
+        case "login-update-password.ftl":
+            return <LoginUpdatePassword kcContext={kcContext} {...rest} />;
+        case "login-update-profile.ftl":
+            return <LoginUpdateProfile kcContext={kcContext} {...rest} />;
+        case "login-idp-link-confirm.ftl":
+            return <LoginIdpLinkConfirm kcContext={kcContext} {...rest} />;
+        case "login-idp-link-email.ftl":
+            return <LoginIdpLinkEmail kcContext={kcContext} {...rest} />;
+        case "login-page-expired.ftl":
+            return <LoginPageExpired kcContext={kcContext} {...rest} />;
+        case "login-config-totp.ftl":
+            return <LoginConfigTotp kcContext={kcContext} {...rest} />;
+        case "logout-confirm.ftl":
+            return <LogoutConfirm kcContext={kcContext} {...rest} />;
+        case "idp-review-user-profile.ftl":
+            return <IdpReviewUserProfile kcContext={kcContext} {...rest} />;
+        case "update-email.ftl":
+            return <UpdateEmail kcContext={kcContext} {...rest} />;
+        case "select-authenticator.ftl":
+            return <SelectAuthenticator kcContext={kcContext} {...rest} />;
+        case "saml-post-form.ftl":
+            return <SamlPostForm kcContext={kcContext} {...rest} />;
+        case "delete-credential.ftl":
+            return <DeleteCredential kcContext={kcContext} {...rest} />;
+        case "code.ftl":
+            return <Code kcContext={kcContext} {...rest} />;
+        case "delete-account-confirm.ftl":
+            return <DeleteAccountConfirm kcContext={kcContext} {...rest} />;
+        case "frontchannel-logout.ftl":
+            return <FrontchannelLogout kcContext={kcContext} {...rest} />;
+        case "login-recovery-authn-code-config.ftl":
+            return <LoginRecoveryAuthnCodeConfig kcContext={kcContext} {...rest} />;
+        case "login-recovery-authn-code-input.ftl":
+            return <LoginRecoveryAuthnCodeInput kcContext={kcContext} {...rest} />;
+        case "login-reset-otp.ftl":
+            return <LoginResetOtp kcContext={kcContext} {...rest} />;
+        case "login-x509-info.ftl":
+            return <LoginX509Info kcContext={kcContext} {...rest} />;
+        case "webauthn-error.ftl":
+            return <WebauthnError kcContext={kcContext} {...rest} />;
+        case "login-passkeys-conditional-authenticate.ftl":
+            return (
+                <LoginPasskeysConditionalAuthenticate kcContext={kcContext} {...rest} />
+            );
+        case "login-idp-link-confirm-override.ftl":
+            return <LoginIdpLinkConfirmOverride kcContext={kcContext} {...rest} />;
+    }
+    assert<Equals<typeof kcContext, never>>(false);
 }
-
-const classes = {} satisfies { [key in ClassKey]?: string };
--- src/login/pages/Info/Info.tsx
+++ src/login/pages/Info/Info.tsx
@@ -3,7 +3,7 @@ import { kcSanitize } from "keycloakify/lib/kcSanitize";
 import type { KcContext } from "../KcContext";
 import type { I18n } from "../i18n";
 
-export default function Info(props: PageProps<Extract<KcContext, { pageId: "info.ftl" }>, I18n>) {
+export function Info(props: PageProps<Extract<KcContext, { pageId: "info.ftl" }>, I18n>) {
     const { kcContext, i18n, doUseDefaultCss, Template, classes } = props;
 
     const { advancedMsgStr, msg } = i18n;
