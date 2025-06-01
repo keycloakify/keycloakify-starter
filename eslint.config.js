89eab325942fa9846ef5151b2d537fd06f079a63
garronej <joseph.garrone@protonmail.com>
2025-06-01 22:34:59 +0200

    Update dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,48 +1,38 @@
-import typescriptEslint from "typescript-eslint";
-import reactRefresh from "eslint-plugin-react-refresh";
-import react from "eslint-plugin-react";
-import reactHooks from "eslint-plugin-react-hooks";
-import eslintConfigPrettier from "eslint-config-prettier";
-import globals from "globals";
-import js from "@eslint/js";
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import prettier from "eslint-config-prettier";
 import storybook from "eslint-plugin-storybook";
 
-export default typescriptEslint.config(
-  js.configs.recommended,
-  ...typescriptEslint.configs.recommended,
-  react.configs.flat.recommended,
-  react.configs.flat["jsx-runtime"],
-  eslintConfigPrettier,
-  ...storybook.configs["flat/recommended"],
-  {
-    ignores: ["dist/**", "public/**"],
-  },
-  {
-    plugins: {
-      "react-refresh": reactRefresh,
-      "react-hooks": reactHooks,
-    },
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-      },
-    },
-    settings: {
-      react: {
-        version: "detect",
-      },
-    },
-    rules: {
-      "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
-      "react-hooks/exhaustive-deps": "off",
-      "@typescript-eslint/no-redeclare": "off",
-      "no-labels": "off",
-    },
-  },
+export default tseslint.config(
+  prettier,
+  storybook,
   {
     files: ["**/*.stories.*"],
     rules: {
       "import/no-anonymous-default-export": "off",
     },
   },
-);
+  { ignores: ['dist'] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+    },
+  },
+)
