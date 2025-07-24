0b5804914f2d3c275621abb46e8a8af04e7d1dc6
Jamie <jamie@otixo.com>
2025-07-24 11:05:46 +0200

    wip

--- src/login/KcPage.tsx
+++ src/login/KcPage.tsx
@@ -4,6 +4,9 @@ import type { KcContext } from "./KcContext";
 import { useI18n } from "./i18n";
 import DefaultPage from "keycloakify/login/DefaultPage";
 import Template from "keycloakify/login/Template";
+
+import "./main.css";
+
 const UserProfileFormFields = lazy(
     () => import("keycloakify/login/UserProfileFormFields")
 );
