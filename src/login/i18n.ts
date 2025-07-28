a462a5cd599604d06fe3682e76126229497b7a66
Jamie <jamie@otixo.com>
2025-07-28 15:32:30 +0200

    wip

--- src/login/i18n.ts
+++ src/login/i18n.ts
@@ -3,7 +3,29 @@ import { i18nBuilder } from "keycloakify/login";
 import type { ThemeName } from "../kc.gen";
 
 /** @see: https://docs.keycloakify.dev/features/i18n */
-const { useI18n, ofTypeI18n } = i18nBuilder.withThemeName<ThemeName>().build();
+const { useI18n, ofTypeI18n } = i18nBuilder.withThemeName<ThemeName>()
+    .withCustomTranslations({
+        en: {
+            loginAccountTitle: "Welcome back! Sign In",
+            loginTitleHtml: "Welcome back! Sign In",
+            noAccount: "You do not have an account?",
+            doRegister: "Sign Up here",
+            doForgotPassword: "Forgot your password?",
+            usernameOrEmail: "Email Address *",
+            password: "Password *"
+          },
+          // cspell: disable
+          de: {
+            loginAccountTitle: "Willkommen zurück! Melde dich an",
+            loginTitleHtml: "Willkommen zurück! Melde dich an",
+            noAccount: "Du hast noch kein Konto?",
+            doRegister: "Kostenlos registrieren",
+            doForgotPassword: "Passwort vergessen?",
+            usernameOrEmail: " E-Mail-Adresse *",
+            password: "Passwort *"
+        }
+        // cspell: enable
+    }).build();
 
 type I18n = typeof ofTypeI18n;
 
