cd6b0af483e1b82311cd4a9ba473ea37dbbb8082
Jamie <jamie@otixo.com>
2025-07-30 09:22:04 +0200

    wip

--- src/login/i18n.ts
+++ src/login/i18n.ts
@@ -6,8 +6,8 @@ import type { ThemeName } from "../kc.gen";
 const { useI18n, ofTypeI18n } = i18nBuilder.withThemeName<ThemeName>()
   .withCustomTranslations({
     en: {
-      loginAccountTitle: "Welcome back! Sign In",
-      loginTitleHtml: "Welcome back! Sign In",
+      // loginAccountTitle: "Welcome back! Sign In",
+      // loginTitleHtml: "Welcome back! Sign In",
       noAccount: "You do not have an account?",
       doRegister: "Sign Up here",
       doForgotPassword: "Forgot your password?",
@@ -16,8 +16,8 @@ const { useI18n, ofTypeI18n } = i18nBuilder.withThemeName<ThemeName>()
     },
     // cspell: disable
     de: {
-      loginAccountTitle: "Willkommen zurück! Melde dich an",
-      loginTitleHtml: "Willkommen zurück! Melde dich an",
+      // loginAccountTitle: "Willkommen zurück! Melde dich an",
+      // loginTitleHtml: "Willkommen zurück! Melde dich an",
       noAccount: "Du hast noch kein Konto?",
       doRegister: "Kostenlos registrieren",
       doForgotPassword: "Passwort vergessen?",
