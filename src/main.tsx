fa6f5eb1adfb65967f9bd9063c3849b6fb1f7acc
Anton Yefremov <7419888+yeahframeoff@users.noreply.github.com>
2025-08-07 12:52:14 +0300

    Remove comment/uncomment hassle in dev/prod mode
    Replace comment/uncomment solution with dynamic import, which keeps bundle optimal, and removes need to remember to keep things commented and uncommented

--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,14 @@ import { createRoot } from "react-dom/client";
 import { StrictMode } from "react";
 import { KcPage } from "./kc.gen";
 
-// The following block can be uncommented to test a specific page with `yarn dev`
-// Don't forget to comment back or your bundle size will increase
-/*
-import { getKcContextMock } from "./login/KcPageStory";
-
 if (import.meta.env.DEV) {
+    const { getKcContextMock } = await import("./login/KcPageStory");
     window.kcContext = getKcContextMock({
         pageId: "register.ftl",
         overrides: {}
     });
 }
-*/
+
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
